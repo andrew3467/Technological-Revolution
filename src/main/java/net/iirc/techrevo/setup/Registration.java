518714eec25eb26f8fb0a81ae3cb20d97bd2f99c
WolfyFX <php115145@gmail.com>
2023-08-13 22:37:54 +0200

    Official ores added

--- src/main/java/net/iirc/techrevo/setup/Registration.java
+++ src/main/java/net/iirc/techrevo/setup/Registration.java
@@ -30,7 +30,50 @@ public class Registration {
     public static final RegistryObject<Block> TEST_ORE = BLOCKS.register("test_ore", () -> new Block(BLOCK_PROPERTIES));
     public static final RegistryObject<Block> COPPER_CABLE = BLOCKS.register("copper_cable", () -> new CopperCable(BLOCK_PROPERTIES));
 
+//  Official ores
 
+    public static final RegistryObject<Block> HEMATITE_ORE = BLOCKS.register("hematite_ore", () -> new Block(BLOCK_PROPERTIES));
+    public static RegistryObject<Item> HEMATITE_ORE_ITEM = fromBlock(HEMATITE_ORE);
+
+    public static final RegistryObject<Block>  BAUXITE_ORE = BLOCKS.register("bauxite_ore", () -> new Block(BLOCK_PROPERTIES));
+    public static RegistryObject<Item> BAUXITE_ORE_ITEM = fromBlock(BAUXITE_ORE);
+
+    public static final RegistryObject<Block> PYRITE_ORE = BLOCKS.register("pyrite_ore", () -> new Block(BLOCK_PROPERTIES));
+    public static RegistryObject<Item> PYRITE_ORE_ITEM = fromBlock(PYRITE_ORE);
+
+
+    public static final RegistryObject<Block> MAGNETITE_ORE  = BLOCKS.register("magnetite_ore", () -> new Block(BLOCK_PROPERTIES));
+    public static RegistryObject<Item> MAGNETITE_ORE_ITEM = fromBlock(MAGNETITE_ORE);
+
+
+
+    public static final RegistryObject<Block>  CHALCOPYRITE_ORE = BLOCKS.register("chalcopyrite_ore", () -> new Block(BLOCK_PROPERTIES));
+    public static RegistryObject<Item> CHALCOPYRITE_ORE_ITEM = fromBlock(CHALCOPYRITE_ORE);
+
+
+
+    public static final RegistryObject<Block> MALECHITE_ORE = BLOCKS.register("malechite_ore", () -> new Block(BLOCK_PROPERTIES));
+    public static RegistryObject<Item> MALCHEITE_ORE_ITEM = fromBlock(MALECHITE_ORE);
+
+
+
+    public static final RegistryObject<Block> SPHALERITE_ORE = BLOCKS.register("sphalerite_ore", () -> new Block(BLOCK_PROPERTIES));
+    public static RegistryObject<Item>  SPHALERITE_ORE_ITEM = fromBlock(SPHALERITE_ORE);
+
+
+
+    public static final RegistryObject<Block> FLOURITE_ORE = BLOCKS.register("flourite_ore", () -> new Block(BLOCK_PROPERTIES));
+    public static RegistryObject<Item> FLOURITE_ORE_ITEM = fromBlock(FLOURITE_ORE);
+
+
+
+
+
+
+
+
+
+    //
 
     public static RegistryObject<Item> TEST_BLOCK_ITEM = fromBlock(TEST_BLOCK);
     public static RegistryObject<Item> TEST_ORE_ITEM = fromBlock(TEST_ORE);
